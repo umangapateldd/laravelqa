dd4e7b643e1abcbd3939504a08eefc6bc2a3f173
vrundavg10 <vrunda.vagadia@devdigital.com>
2020-02-24 14:16:50 +0530

    AFD-94-Front > Added code for Our team

--- src/main/java/com/pages/frontpages/Frontourteam.java
+++ src/main/java/com/pages/frontpages/Frontourteam.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import com.basicactions.DropDownHelper;
+import com.basicactions.ExcelHelper;
 import com.basicactions.LogHelper;
 import com.utilities.CommonFunc;
 import com.utilities.CommonVariables;
+import com.utilities.FilesPaths;
 
 public class Frontourteam {
 
@@ -31,10 +33,13 @@ public class Frontourteam {
 
 	public void verifyOurteamtitle() throws InterruptedException {
 		log.info("********************Enter the Title********************");
+		ExcelHelper.readDataFromXLS(FilesPaths.excel_data_file_name, CommonVariables.ourteam);
+		String linkdin = ExcelHelper.getData(1, 3);
 
 		List<WebElement> ele = driver.findElements(By.xpath("//*[@id='team']/div/div[2]/div"));
 		int count = ele.size();
 		System.out.println("element size= " + count);
+		boolean ourteamVal = false;
 
 		for (int i = 1; i <= count; i++) {
 			List<WebElement> memberlink = driver
@@ -56,20 +61,67 @@ public class Frontourteam {
 					driver.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/h5/a")).click();
 					Thread.sleep(2000);
 					System.out.println("Detail screen is open");
-
+					ourteamVal = true;
 					break;
-				} 
+				}
 			} else if (membercount > 0) {
 				if (driver.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/h5")).getText()
 						.equals(CommonVariables.txtSearchCmnVar)) {
 					System.out.println("Verify the value = " + driver
 							.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/h5")).getText());
-					driver.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/picture/img")).click();
-					System.out.println("Click on Image");
+					driver.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/div[1]")).getText();
+					System.out.println("Verify the position value ="
+							+ driver.findElement(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/div[1]"))
+									.getText());
+//					System.out.println("check value of size " + driver
+//							.findElements(By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/div[2]/a/span"))
+//							.size());
 
-					break;
-				} 
+					System.out.println("Linkdin value for Excel =" + linkdin.isEmpty());
+
+					if (linkdin.isEmpty()) {
+						System.out.println("Linkdin value is not found in Excel sheet");
+
+						if (driver
+								.findElements(
+										By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/div[2]/a/span"))
+								.size() > 0) {
+
+							Thread.sleep(2000);
+							System.out.println("Linkdin icon is found");
+							assert false;
+
+						} else {
+							assert true;
+						}
+					} else {
+						if (driver
+								.findElements(
+										By.xpath("//*[@id='team']/div/div[2]/div[" + i + "]/div/div/div[2]/a/span"))
+								.size() > 0) {
+
+							Thread.sleep(2000);
+							System.out.println("Linkdin icon is found");
+							assert true;
+
+						} else {
+							assert false;
+						}
+					}
+				}
+
+				ourteamVal = true;
+				break;
+
+			} else {
+				assert false;
 			}
+		}
+		if (ourteamVal == true) {
+			System.out.println("IF CONIDITION IS TRUE");
+
+		} else {
+			assert false;
 		}
 	}
 }
